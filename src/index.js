8f9071251ffe81d918253b670661ea56530d3e54
Cameron Yick <cameron.yick@datadoghq.com>
2019-02-06 14:16:47 -0500

    [prototyping] add 2d brush to the minimap

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { scaleTime, scaleLinear, select, line, event } from "d3";
 
 import { axisBottom, axisLeft } from 'd3-axis';
 import { extent, max } from "d3-array";
-import { brushX, brushY, brushSelection } from "d3-brush";
+import { brush, brushX, brushY, brushSelection } from "d3-brush";
 import { zoom } from "d3-zoom";
 
 import { zipWith } from "lodash";
@@ -15,31 +15,35 @@ import { zipWith } from "lodash";
 import { fromEvent, } from 'rxjs';
 import { map, buffer, debounceTime, filter } from 'rxjs/operators';
 
-// Fixtures/constants
+// Fixtures/constants for main chart
 const height = 550;
 const width = 900;
-const margin = { top: 20, right: 30, bottom: 130, left: 30 };
+const margin = { top: 30, right: 30, bottom: 50, left: 30 };
 
 const DEBUG = false;
 
 const LOG = (msg) => DEBUG && console.log(msg);
 
 // Dimensional Bounds
-const xMin = margin.left;
-const xMax = width - margin.right;
+const getLayout = ({width, height, margin}) => {
+  const xMin = margin.left;
+  const xMax = width - margin.right;
+  const yMax = margin.top;
+  const yMin = height - margin.bottom;
 
-const yMax = margin.top;
-const yMin = height - margin.bottom;
-const LAYOUT = {
-  height,
-  width,
-  margin,
-  // Bounds
-  xMin,
-  xMax,
-  yMin,
-  yMax,
-};
+  return {
+      height,
+      width,
+      margin,
+      // Bounds
+      xMin,
+      xMax,
+      yMin,
+      yMax,
+  }
+}
+
+const LAYOUT = getLayout({width, height, margin});
 
 
 const DATA = mockData.map(series => {
@@ -67,10 +71,10 @@ const doubleClick$ = mouse$.pipe(
 
 /**
  * @param dataset: a list of Series, where a series is a list of Points. Point has x and y props.
- * @param target: a d3-selection of a node to attach things to.
+ * @param node: a d3-selection of a node to attach things to.
  * @param layout: object with data relating to pixels / page layout, rather than data values
  */
-const drawChart = (target, dataset, layout) => {
+const drawChart = (node, dataset, layout) => {
   // TODO: make this true across all series, not just the first
   // Flat array across all series
   const data = dataset[0];
@@ -88,7 +92,7 @@ const drawChart = (target, dataset, layout) => {
     .range([layout.yMin, layout.yMax]);
 
   // Mutate the DOM
-  const svg = target
+  const svg = node
     .append("svg")
     .attr("height", layout.height)
     .attr("width", layout.width)
@@ -206,7 +210,7 @@ const drawChart = (target, dataset, layout) => {
     .append("rect")
     .attr("width", layout.xMax - layout.xMin)
     .attr("height", layout.yMin) // Clip the Y axis
-    .attr("transform", `translate(${xMin},0)`);
+    .attr("transform", `translate(${layout.xMin},0)`);
 
   // Y axis Brush
   const yBrush = brushY()
@@ -241,7 +245,7 @@ const drawChart = (target, dataset, layout) => {
   }
 };
 
-// Draw the chart
+// Draw the main line chart
 const mainChart = drawChart(select("#app"), D3_DATA, LAYOUT);
 
 // Command: has side effect
@@ -261,3 +265,136 @@ const resetLineChart = (chart) => {
 doubleClick$.subscribe(() => {
   resetLineChart(mainChart);
 });
+
+// PART 2
+// Add a minimap! which is a tiny brushable chart for controlling another chart.
+/**
+ * @param dataset: a list of Series, where a series is a list of Points. Point has x and y props.
+ * @param node: a d3-selection of a node to attach things to.
+ * @param layout: object with data relating to pixels / page layout, rather than data values
+ * @param targetChart: the chart to control
+ */
+const drawMinimap = (node, dataset, layout, targetChart) => {
+  // TODO: make this true across all series, not just the first
+  // Flat array across all series
+  const data = dataset[0];
+  const xDomain = extent(data, d => d.x);
+  const yDomain = [0, max(data, d => d.y)];
+
+  // Scaling functions
+  let xScale = scaleTime()
+    .domain(xDomain)
+    .range([layout.xMin, layout.xMax]);
+
+  const yScale = scaleLinear()
+    .domain(yDomain)
+    .nice()
+    .range([layout.yMin, layout.yMax]);
+
+  // Mutate the DOM
+  const svg = node
+    .append("svg")
+    .attr("height", layout.height)
+    .attr("width", layout.width)
+    .attr("class", "bg");
+  ;
+
+  // Axes
+  const onXZoom = function () {
+    xScale = event.transform.rescaleX(xScale);
+    xAxisGroup.call(xAxis);
+    lines.attr("d", lineGenerator);
+  };
+
+  const xAxis = g => g // TODO: should this translate be hoisted somewhere
+    .attr("transform", `translate(0,${layout.yMin})`)
+    .call(axisBottom(xScale)
+      .ticks(width / 80)
+      .tickSizeOuter(0))
+    .call(zoom()  // Zoom is a little speedy, but not worth re-implementing current x-axis dragging logic
+      .on("zoom", onXZoom));
+
+  // DATA FUNCTION
+  const lineGenerator = line()
+    .defined(d => !isNaN(d.y))
+    .x(d => xScale(d.x))
+    .y(d => yScale(d.y));
+
+  // Draw line for each series
+  const lineContainer = svg
+    .append("g")
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1.5)
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-linecap", "round");
+  const lines = lineContainer
+    .selectAll(".lineSeries") // Don't grab axis by accident
+    .data(dataset)
+    .enter()
+    .append("path")
+    .attr("class", "lineSeries")
+    .attr("d", lineGenerator);
+
+
+
+  // Interactions - an XY Brush
+  const brushedTwoDimensional = function () {
+    console.log("Minimap 2d Brush Triggered");
+    const selection = brushSelection(this);
+    if (selection === null) return;
+
+    const [topLeftCorner, bottomRightCorner] = selection;
+    const xSelection = [topLeftCorner[0], bottomRightCorner[0]];
+    const ySelection = [topLeftCorner[1], bottomRightCorner[1]];
+
+    console.log({xSelection, ySelection});
+
+    // Then, we'll redraw the x dimension
+    const newXDomain = xSelection.map(xScale.invert, xScale);
+    targetChart.xScale.domain(newXDomain);
+
+    console.log(newXDomain)
+
+    ySelection.reverse();
+    const newYDomain = ySelection.map(yScale.invert, yScale);
+    targetChart.yScale.domain(newYDomain);
+
+    targetChart.lines
+      .attr("d", targetChart.lineGenerator);
+
+    // Redraw appropriate axis
+    targetChart.xAxisGroup.call(targetChart.xAxis);
+    // targetChart.yAxisGroup.call(targetChart.yAxis);
+// Remove the brush after zooming
+  };
+  const twoDimensionalBrush = brush()
+    .extent([[layout.xMin, layout.yMax], [layout.xMax, layout.yMin]])
+    .on('end', brushedTwoDimensional);
+
+
+
+  const twoDimensionalBrushGroup = svg
+    .append('g')
+    .attr('class', 'twoDimensionalBrush')
+    .call(twoDimensionalBrush);
+
+  return {
+    xDomain,
+    yDomain,
+    lineGenerator,
+    xScale,
+    yScale,
+    lines
+  }
+};
+
+
+const minimapMargin = { top: 0, right: 30, bottom: 0, left: 30 };
+
+const minimapLayout = getLayout({
+  width: 900,
+  height: 80,
+  margin: minimapMargin
+});
+const miniMap = drawMinimap(select("#minimap"), D3_DATA, minimapLayout, mainChart);
